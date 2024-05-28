fe7745665db403b07346fbb5628b3759b068875b
320506201 <3256783093@qq.com>
2024-05-28 18:49:08 +0800

    无

--- Python_actualcombat/Time_load.py
+++ Python_actualcombat/Time_load.py
@@ -1,33 +1,23 @@
 import turtle
+import time
 
 T = turtle.Turtle()
-T.speed(7)
 T.pensize(5)
 T.pencolor("red")
+T.speed(0)
+a = -100
 
-while True:
-    T.penup()
-    T.goto(-50,-100)
-    T.pendown()
-    T.circle(100)
-    T.clear()
-    T.penup()
-    T.goto(0,-100)
-    T.pendown()
-    T.circle(100)
-    T.clear()
-    T.penup()
-    T.goto(50,-100)
-    T.pendown()
-    T.circle(100)
-    T.clear()
-    T.penup()
-    T.goto(100,-100)
-    T.pendown()
-    T.circle(100)
-    T.clear()
-    T.penup()
-    T.goto(-100,-100)
-    T.pendown()
-    T.circle(100)
-    T.clear()
+def draw_circle():
+    global a
+    while True:
+        while a != 100:
+            T.penup()
+            T.goto(a,-100)
+            T.pendown()
+            T.circle(100)
+            T.clear()
+            a +=1
+            time.sleep(0.05)
+        a =-100
+
+draw_circle()
